2f6ba1805167b18963c7290d4e735a14e34011fa
fatemehghazavi@gmail.com <7214nf7214>
2022-03-06 13:01:33 +0330

    props definition

--- src/App.js
+++ src/App.js
@@ -3,28 +3,55 @@ import './App.css';
 import ExpenseItem from "./component/ExpenseItem";
 
 function App() {
-  return (
-    <div className="App">
-       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 450,
+            date: new Date(2021, 5, 12),
+        },
+    ];
 
-        <ExpenseItem></ExpenseItem>
+    return (
+       <div>
+            <ExpenseItem
+                title={expenses[0].title}
+                amount={expenses[0].amount}
+                    date={expenses[0].date}
+                    ></ExpenseItem>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+            <ExpenseItem
+                    title={expenses[1].title}
+                    amount={expenses[1].amount}
+                    date={expenses[1].date}
+                    ></ExpenseItem>
+            <ExpenseItem
+                    title={expenses[2].title}
+                    amount={expenses[2].amount}
+                    date={expenses[2].date}
+                    ></ExpenseItem>
+            <ExpenseItem
+                    title={expenses[3].title}
+                    amount={expenses[3].amount}
+                    date={expenses[3].date}>
+            </ExpenseItem>
 
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 
-      </header>
-      <ExpenseItem></ExpenseItem>
-    </div>
-
-  );
+        </div>
+    );
 }
 
 export default App;
